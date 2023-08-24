15b262c28ae4ecb9561ebb23230566f558873f36
Nahuel1798 <nahuellucero25@gmail.com>
2023-08-24 16:26:23 -0300

    Initial commit

---
+++ src/main/java/aplicacion/Producto.java
@@ -0,0 +1,44 @@
+
+package aplicacion;
+
+/**
+ *
+ * @author Nahue
+ */
+class Producto {
+    private String nombre;
+    private String categoria;
+    private String precio;
+
+    public Producto(String nombre, String categoria, String precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+    
+    
+}
